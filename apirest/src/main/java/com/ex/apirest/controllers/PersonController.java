940e6de9ed4c4f44f37e18b97633d33365f06d76
IvoPavi <ivo_rola@hotmail.com>
2023-12-26 15:17:36 -0300

    se agregaron metodos listar y borrar, se modifico el metodo create

--- apirest/src/main/java/com/ex/apirest/controllers/PersonController.java
+++ apirest/src/main/java/com/ex/apirest/controllers/PersonController.java
@@ -3,10 +3,9 @@ package com.ex.apirest.controllers;
 import com.ex.apirest.model.Person;
 import com.ex.apirest.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -15,7 +14,12 @@ public class PersonController {
     @Autowired
     PersonService personService;
     @PostMapping("/create")
-    public void create(@RequestBody Person person){
-        personService.createPerson(person);
+    public Person create(@RequestBody Person person){
+        return personService.createPerson(person);
+
+    }
+    @GetMapping("/persons")
+    public List<Person> list(){
+        return personService.showPerson();
     }
 }
--- apirest/src/main/java/com/ex/apirest/services/PersonService.java
+++ apirest/src/main/java/com/ex/apirest/services/PersonService.java
@@ -5,15 +5,22 @@ import com.ex.apirest.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PersonService {
 
     @Autowired
     PersonRepository personRepository;
-    public void createPerson(Person person){
+    public Person createPerson(Person person){
         personRepository.save(person);
+        return person;
     }
 
+    public List<Person> showPerson(){
+        return personRepository.findAll();
+
+    }
 
 }
 
